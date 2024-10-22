516caff1a3f12cb78427e74d743b7127bbc75dd5
Waghre Abhishek <waghreabhishek@gmail.com>
2024-10-22 14:22:53 +0530

    backend Setup,

--- LamaAI-Clone/src/components/newPrompt/NewPrompt.jsx
+++ LamaAI-Clone/src/components/newPrompt/NewPrompt.jsx
@@ -1,30 +1,59 @@
 import React from 'react'
 import { useEffect, useRef, useState } from "react";
 import './NewPrompt.css'
-
+import Upload from '../uploads/upload';
+import { IKImage } from 'imagekitio-react';
 
 
 function NewPrompt() {
 
     const endRef = useRef(null)
 
-  useEffect(() =>{
-     endRef.current.scrollIntoView({behavior: "smooth"})
-  }, [])
-    
+    const [img, setImg] = useState({
+        isLoading: false,
+        error: "",
+        dbData: {},
+    })
+
+    useEffect(() => {
+        endRef.current.scrollIntoView({ behavior: "smooth" })
+    }, [])
+
     return (
         <>
+            {
+                img.isLoading && <div>
+                    Looding...
+                </div>
+            }
+
+            {/* Fetch the Image and show on Screen */}
+            {
+
+                img.dbData?.filePath && (
+                    <IKImage
+                        urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT_KEY}
+                        path={img.dbData?.filePath}
+                        width="380"
+                        transformation={[{ width: 380 }]}
+                    />
+                )
+            }
+
             <div className='endChat' ref={endRef}></div>
-                <form className="newForm" >
-                    <label htmlFor='file'>
-                        <img src='/attachment.png'></img>
-                    </label>
-                        <input id="file" type="file" multiple={false} hidden /> 
-                    <input type="text" name="text" placeholder="Ask anything..." />
-                    <button>
-                        <img src="/arrow.png" alt="" />
+            <form className="newForm" >
+
+                {/* Uplaod Component here */}
+                <Upload setImg={setImg} />
+
+                <input id="file" type="file" multiple={false} hidden />
+                <input type="text" name="text" placeholder="Ask anything..." />
+
+                
+                <button>
+                    <img src="/arrow.png" alt="" />
                 </button>
-                </form> 
+            </form>
         </>
     )
 }
